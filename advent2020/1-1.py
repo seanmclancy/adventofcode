5e6d2894ec7951accb0a98f809b423b21505fb21
Sean Clancy <seanmclancy@gmail.com>
2020-12-01 15:32:15 -0700

    Initial commit for day 1

---
+++ advent2020/1-1.py
@@ -0,0 +1,10 @@
+import math
+
+f = open("inputs\\day1.txt", "r")
+xp = list(map(int, f.read().splitlines()))
+
+for x in xp:
+    for y in xp:
+        for z in xp:
+            if (x + y + z) == 2020:
+                print(x, y, z, x*y*z)
